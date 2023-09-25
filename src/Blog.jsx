4ce823069cffc33baa12659d3c900a662bf271b5
antiblockchain <57572382+antiblockchain@users.noreply.github.com>
2023-09-25 08:45:41 -0500

    Fixed routing and CORS

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -6,7 +6,7 @@ import { BlogCard } from "./components/Card";
 import { BlogPost } from "./components/BlogPost";
 import Axios from "axios";
 
-const SERVER_URL = "http://localhost:2999";
+const SERVER_URL = "https://elynch.co:2999";
 
 export function Blog () {
     const [blogs, setBlogs] = useState([]);
